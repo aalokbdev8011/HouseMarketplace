21500dfb86d7526270c6291a7f234f871e2ffc2d
Alok <alokdev80@gmail.com>
2023-10-30 15:47:54 +0530

    add image url in property

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -3,7 +3,7 @@
 class PropertySerializer
   include JSONAPI::Serializer
   attributes :id, :title, :price, :city, :district, :address, :mrt_station, :property_type, :rooms, :created_at,
-             :updated_at, :is_favorite
+             :updated_at, :is_favorite, :image_url
 
   attribute :image do |object|
     Rails.application.routes.url_helpers.rails_blob_url(object.image.blob, only_path: true) if object.image.attached?
